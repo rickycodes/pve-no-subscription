666943824225166b5c92b041775b142149201ced
rickycodes <ricky.miller@gmail.com>
2020-11-23 15:52:10 -0500

    only run the replacement if it's needed

--- no-subscription-warning.sh
+++ no-subscription-warning.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
+FIND="NotFound"
+REPLACE="Active"
 FILE=/usr/share/perl5/PVE/API2/Subscription.pm
-if test -f "$FILE"; then
-    sed -i "s/NotFound/Active/g" "$FILE"
-    ex=$?
-    if [ "$ex" -eq "0" ]; then
-      systemctl restart pvedaemon && systemctl restart pveproxy
-      echo "all done!"
-    else
-      echo "something went wrong :("
-    fi
+
+CAT=$(cat "$FILE" | grep "$FIND")
+
+if [ $? -eq "0" ]; then
+  echo "subscription status: $FIND"
+  if test -f "$FILE"; then
+      echo "performing replacement..."
+      sed -i "s/$FIND/$REPLACE/g" "$FILE"
+      ex=$?
+      if [ "$ex" -eq "0" ]; then
+        echo "restarting services..."
+        systemctl restart pvedaemon && systemctl restart pveproxy
+        echo "all done!"
+      else
+        echo "something went wrong :("
+      fi
+  else
+    echo "file does not exist! are you sure this is pve?"
+  fi
 else
-  echo "file does not exist! are you sure this is pve?"
+  echo "cannot find item. have you already run the replacement?"
 fi
